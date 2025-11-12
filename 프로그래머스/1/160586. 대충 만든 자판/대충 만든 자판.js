b873d292ec38f8441b516ae7a5a40902a065a89c
고영준 <53961747+yeongjunekoh@users.noreply.github.com>
2025-11-12 18:00:15 +0900

    [level 1] Title: 대충 만든 자판, Time: 1.88 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.js
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.js
@@ -1,28 +1,11 @@
 function solution(keymap, targets) {
-  const A = 'A'.charCodeAt(0);
-  const cost = Array(26).fill(Infinity);
-
-  // 전처리: 문자별 최소 클릭수
-  for (const key of keymap) {
-    for (let i = 0; i < key.length; i++) {
-      const idx = key.charCodeAt(i) - A;
-      // 대문자만 주어진다고 했으므로 범위 체크 생략 가능
-      if (i + 1 < cost[idx]) cost[idx] = i + 1;
+    const answer = [];
+    const map = {}
+    for (const items of keymap) {
+        items.split('').map((item, index) => map[item] = (map[item] < index+1 ? map[item] : index+1))
     }
-  }
-
-  // 계산: 각 target 합산
-  const answer = [];
-  for (const t of targets) {
-    let sum = 0;
-    let possible = true;
-    for (let i = 0; i < t.length; i++) {
-      const idx = t.charCodeAt(i) - A;
-      const c = cost[idx];
-      if (c === Infinity) { possible = false; break; }
-      sum += c;
+    for (const items of targets) {
+        answer.push(items.split('').reduce((cur, item) => cur += map[item], 0) || -1)
     }
-    answer.push(possible ? sum : -1);
-  }
-  return answer;
+    return answer;
 }
