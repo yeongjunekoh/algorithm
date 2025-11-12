e080ae52add82b756b4e8783a43a1726f2021389
고영준 <53961747+yeongjunekoh@users.noreply.github.com>
2025-11-12 17:59:21 +0900

    [level 1] Title: 대충 만든 자판, Time: 0.95 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.js
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.js
@@ -1,24 +1,28 @@
 function solution(keymap, targets) {
-    const MAX_LENGTH_OF_KEY_MAP = 101;
-    var answer = [];
-    
-    targets.forEach((target) => {
-        let numberOfPress = 0;
-        
-        for(const alphabet of target) {
-            let min = MAX_LENGTH_OF_KEY_MAP;
-            keymap.forEach((key) => {
-                const keyIndex = key.indexOf(alphabet);
-                if(keyIndex >= 0) min = Math.min(min, keyIndex + 1);
-            })
-            if(min === MAX_LENGTH_OF_KEY_MAP) {
-                numberOfPress = -1;
-                break;
-            }
-            numberOfPress += min;
-        }
-        
-        answer.push(numberOfPress);
-    })
-    return answer;
+  const A = 'A'.charCodeAt(0);
+  const cost = Array(26).fill(Infinity);
+
+  // 전처리: 문자별 최소 클릭수
+  for (const key of keymap) {
+    for (let i = 0; i < key.length; i++) {
+      const idx = key.charCodeAt(i) - A;
+      // 대문자만 주어진다고 했으므로 범위 체크 생략 가능
+      if (i + 1 < cost[idx]) cost[idx] = i + 1;
+    }
+  }
+
+  // 계산: 각 target 합산
+  const answer = [];
+  for (const t of targets) {
+    let sum = 0;
+    let possible = true;
+    for (let i = 0; i < t.length; i++) {
+      const idx = t.charCodeAt(i) - A;
+      const c = cost[idx];
+      if (c === Infinity) { possible = false; break; }
+      sum += c;
+    }
+    answer.push(possible ? sum : -1);
+  }
+  return answer;
 }
