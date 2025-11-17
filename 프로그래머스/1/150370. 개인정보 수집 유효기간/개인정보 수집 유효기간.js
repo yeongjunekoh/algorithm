1d21372117115358dd35236b99875da65ef8f6a3
고영준 <53961747+yeongjunekoh@users.noreply.github.com>
2025-11-17 18:38:23 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 0.64 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
@@ -1,43 +1,13 @@
 function solution(today, terms, privacies) {
-    const LAST_DAY_INDEX = 27;
-    const LAST_MONTH_INDEX = 11;
+    const toDays = (dateStr) => {
+        const [y,m,d] = dateStr.split('.').map(Number);
+        return y*12*28 + m*28 +d;
+    }
+    
+    const todayDays = toDays(today);
     
     const termsMap = {};
-    const todayArray = today.split('.').map((item) => Number(item));
     var answer = [];
-    
-    const calculateExpireDate = (createAt, period) => {
-        let [year, month, date] = createAt.split('.').map((item) => Number(item) - 1);
-        month += period;
-        date--;
-        
-        if(date < 0) {
-            month--;
-            date = LAST_DAY_INDEX;
-        }
-        
-        if(month < 0) {
-            year--;
-            month = LAST_MONTH_INDEX;
-        }
-        
-        if(month >= 12) {
-            year = year + Math.floor(month/12);
-            month = month%12
-        }
-        return [year+1, month+1, date+1];
-    }
-    
-    const shouldExpire = (currentDt, expiredDt) => {
-        const [currentYear, currentMonth, currentDate] = currentDt;
-        const [expiredYear, expiredMonth, expiredDate] = expiredDt;
-        
-        if(expiredYear < currentYear) return true;
-        if(expiredYear === currentYear && expiredMonth < currentMonth) return true;
-        if(expiredYear === currentYear && expiredMonth === currentMonth && expiredDate < currentDate) return true;
-        
-        return false;
-    }
     
     terms.forEach((item) => {
         const [term, period] = item.split(' ');
@@ -46,7 +16,9 @@ function solution(today, terms, privacies) {
     
     privacies.forEach((item, index) => {
         const [createdAt, term] = item.split(' ');
-        if(shouldExpire(todayArray, calculateExpireDate(createdAt, termsMap[term]))) answer.push(index + 1);
+        const expireDay = toDays(createdAt) + termsMap[term] * 28;
+        
+        if(expireDay <= todayDays) answer.push(index + 1);
     })
     
     
