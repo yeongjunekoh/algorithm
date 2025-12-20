5173f0ddc9f2b39bebe1b542eb6885470903def0
고영준 <53961747+yeongjunekoh@users.noreply.github.com>
2025-12-20 09:01:37 +0900

    [level 1] Title: 기사단원의 무기, Time: 96.30 ms, Memory: 36.5 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.js
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.js
@@ -3,8 +3,9 @@ function solution(number, limit, power) {
     
     for(let i = 1; i <= number; i++){
         let divisor = 0;
-        
-        for (let divider = 1; divider <= Math.sqrt(i); divider++){
+        const root = Math.floor(Math.sqrt(i));
+
+        for (let divider = 1; divider <= root; divider++){
             if(i%divider === 0) {
                 if(divider*divider === i) divisor++;
                 else divisor += 2;
