50d49fb8a031e74f9a87abc183b1aba18715515b
고영준 <53961747+yeongjunekoh@users.noreply.github.com>
2025-12-31 11:30:42 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.24 ms, Memory: 33.1 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.js
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.js
@@ -1,7 +1,8 @@
 function solution(food) {
-    var answer = '';
-    return food.reduce((acc, cur, index) => {
+    const foodString = food.reduce((acc, cur, index) => {
         const foodNumberString = String(index).repeat(Math.floor(cur/2));
-        return acc.replace('0', foodNumberString + '0' + foodNumberString);
-    } ,'0');
+        return acc + foodNumberString;
+    } ,'');
+    
+    return foodString + '0' + [...foodString].reverse().join('');
 }
